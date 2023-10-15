733daa12ff02ef92f0c0b5dc5e530f47f125ec6e
Wenjun <yw.anny2016@gmail.com>
2023-10-14 22:31:18 -0400

    format adjusted

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.views import generic
 
 # Create your views here.
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -13,13 +14,16 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.order_by("-pub_date")[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -27,18 +31,17 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
-        return render(request, 
-                      "polls/detail.html", 
-                      {
-                        "question": question,
-                        "error_message": "You didn't select a choice",
-                        },
-                    )
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice",
+            },
+        )
     else:
         selected_choice.votes += 1
         selected_choice.save()
 
     return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    #reverse() will return a string like "polls/3/results/"
-
-
+    # reverse() will return a string like "polls/3/results/"
